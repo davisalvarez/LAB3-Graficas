0d5fdf8691c3e6f02874947596da80fc681f0eab
davisalvarez <davisen321@gmail.com>
2020-10-26 17:54:49 -0600

    Add files via upload

---
+++ Textures.py
@@ -0,0 +1,9 @@
+import pygame
+
+textures = {
+    '1': pygame.image.load('textures/grad1.png'),
+    '2': pygame.image.load('textures/grad2.png'),
+    '3': pygame.image.load('textures/grad3.png'),
+    '4': pygame.image.load('textures/grad4.png'),
+    '5': pygame.image.load('textures/grad5.png')
+}
